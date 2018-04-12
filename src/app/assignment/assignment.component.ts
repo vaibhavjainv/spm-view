576fdbc07339b72d9180cd949224fb0b13e1de43
Vaibhav Jain <vaibhavj@JainVMBPro-2.local>
2018-04-12 17:04:10 -0400

    Added more functions

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ResourcesService } from '../resources.service';
 
-import { Resource } from '../resource' 
+import { Resource } from '../resource'
 import { Assignment } from '../assignment';
 
 
@@ -15,19 +15,27 @@ export class AssignmentComponent implements OnInit {
 
   assignments: Assignment[];
   blue = "Green";
+  formData = { "projects": [{ "rate": "", "role": "", "account": "ABC", "assignment": "XYZ" }] };
 
   constructor(private resourcesService: ResourcesService) { }
 
   getResources(): void {
     this.resourcesService.getAssignments()
-        .subscribe(resp => this.assignments = resp);
-}
-
-
-
+      .subscribe(resp => this.assignments = resp);
+  }
 
   ngOnInit() {
     this.getResources();
+  }
+
+  onSubmit() {
+    this.resourcesService.addResource(this.formData)
+      .subscribe(resp => this.assignments = resp);
+  }
+
+  deleteResource(id) {
+    this.resourcesService.deleteResource(id)
+      .subscribe(resp => this.assignments = resp);
   }
 
 }
--- src/app/resources.service.ts
+++ src/app/resources.service.ts
@@ -9,12 +9,19 @@ import { Assignment } from './assignment'
 
 import { print } from 'util';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Injectable()
 export class ResourcesService {
 
   private getall = 'http://localhost:3000/api/resources/getall';
   private getallassignmentsURL = 'http://localhost:3000/api/assignments/getall';
+  private addResourceURL = 'http://localhost:3000/api/resources/add';
+  private delResourceURL = 'http://localhost:3000/api/resources/delete';
   
   constructor(private http: HttpClient) { }
 
@@ -25,6 +32,16 @@ export class ResourcesService {
   getAssignments(): Observable<Assignment[]> {
     return this.http.get<Assignment[]>(this.getallassignmentsURL);
   }
+
+  addResource(formData): Observable<Assignment[]> {
+    return this.http.post<Assignment[]>(this.addResourceURL,formData,httpOptions);
+  }
+
+  deleteResource(id): Observable<Assignment[]> {
+    const url = `${this.delResourceURL}/${id}`;
+    return this.http.delete<Assignment[]>(url,httpOptions);
+  }
+
 }
 
 

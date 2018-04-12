943784e132b23a0fb7baa21916167c83ed61adae
Vaibhav Jain <vaibhavj@JainVMBPro-2.local>
2018-04-11 22:00:23 -0400

    Assignments code

---
+++ src/app/assignment.ts
@@ -0,0 +1,20 @@
+export class Assignment {
+    name: String;
+    account: String;
+    resources: [
+        {
+            name: String;
+            location: String;
+            rate: Number;
+            role: String;
+            allocation: [
+                {
+                    week: Date,
+                    hours: Number
+                }
+            ]
+        }
+    ]
+    startWeek: Date;
+    endWeek: Date;
+}

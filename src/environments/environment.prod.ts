85faa480f3c34fffcf8ff9901612d7ed9e76e891
Vaibhav Jain <vaibhavj@JainVMBPro-2.local>
2018-04-18 18:11:33 -0400

    Fixed hostname

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  spmapihost: "internal-spm-api-1972801684.us-west-2.elb.amazonaws.com"
+  spmapihost: "spm-api-749052067.us-west-2.elb.amazonaws.com"
 };

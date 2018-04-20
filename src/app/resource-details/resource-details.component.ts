e9849f47d476e5cc1f476a40ccec7ec7a84bfe38
Vaibhav Jain <vaibhavj@JainVMBPro-2.local>
2018-04-19 23:52:13 -0400

    resource details

--- src/app/resource-details/resource-details.component.ts
+++ src/app/resource-details/resource-details.component.ts
@@ -13,8 +13,9 @@ export class ResourceDetailsComponent implements OnInit {
   resourceInfo = {};
 
   constructor(private route: ActivatedRoute, 
-    private resourcesService: ResourcesService
-  ) { }
+    private resourcesService: ResourcesService,
+  ) { 
+    }
 
   ngOnInit() {
     this.resourceid = this.route.snapshot.paramMap.get('id');
@@ -26,5 +27,35 @@ export class ResourceDetailsComponent implements OnInit {
       .subscribe(resp => this.resourceInfo = resp);
   }
 
+  findmatch(project, week){
+    var mapping = project.allocation.filter(allocation => allocation.week == week);
+    if(mapping!=undefined && mapping.length > 0){
+      return mapping[0].hours;
+    }else{
+      return 0;
+    }
+  }
+
+  findtotalhoursforweek(week,totalhoursmap){
+    for (var i in totalhoursmap){
+      var dt = new Date(week);
+      var idt = new Date(i);
+      if(dt.toDateString()==idt.toDateString()){
+        return totalhoursmap[i];
+      }
+    };
+  }
+
+  findtotalhrcellclass(week,totalhoursmap){
+    for (var i in totalhoursmap){
+      var dt = new Date(week);
+      var idt = new Date(i);
+      if(dt.toDateString()==idt.toDateString()){
+        if(totalhoursmap[i] > 40){
+          return 'extra-hours';
+        }
+      }
+    };
+  }
 
 }
